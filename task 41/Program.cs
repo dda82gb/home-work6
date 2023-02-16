3693ec39726889730ca33a34355716527512a814
Davran_Djumanov <dda_82@mail.ru>
2023-02-16 18:27:08 +0600

    add task41

---
+++ task 41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write($"Введи число М(количество чисел): ");
+int m =int.Parse(Console.ReadLine()!)!;
+int[] massNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    massNumbers[i] =int.Parse(Console.ReadLine()!)!;
+  }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(m);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($" {Comparison(massNumbers)} из них больше 0");
